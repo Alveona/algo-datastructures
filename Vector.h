1cbe80e72c29fc690c504c4edb88eca8d3839093
Alveona <pomavau@yandex.ru>
2018-08-24 18:01:15 +0300

    Constructor, insert and push_back for vector

--- Vector.h
+++ Vector.h
@@ -4,15 +4,19 @@
 
 #ifndef DATA_S_VECTOR_H
 #define DATA_S_VECTOR_H
+
+#include <cstdlib>
+
 typedef int uint;
 template<typename T>
 class Vector
 {
     void extendByAddition(uint value);
-    void extendByMultiplication(uint constant);
-    T* array;
+    void extendByMultiplication(float constant);
+    T* array = nullptr;
     uint size = 0;
-
+    uint reserved = 0;
+    const float extendingConstant = 1.5;
 public:
     Vector(uint size);
     bool empty() const;
@@ -33,5 +37,37 @@ public:
         Vector::size = size;
     }
 };
+
+
+template<typename T>
+Vector<T>::Vector(uint size)
+{
+    array = malloc(size * sizeof(T));
+    if(array == nullptr)
+        throw "Malloc failed";
+    this->size = size;
+    reserved = size;
+}
+
+template <typename T>
+void Vector<T>::insert(uint position, T data) {
+    if(position < 0 || position >= size)
+        throw "Index out of bounds";
+    array[position] = data;
+    size++;
+    if(size == reserved)
+        extendByMultiplication(extendingConstant);
+}
+
+template <typename T>
+void Vector<T>::push_back(T data) {
+    array[size - 1] = data;
+    size++;
+    if(size == reserved)
+        extendByMultiplication(extendingConstant);
+}
+
+
+
 
 #endif //DATA_S_VECTOR_H
